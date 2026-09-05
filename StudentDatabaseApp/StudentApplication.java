a5142bcf9ee429d3e1a2abf45b5b907734263894
rajveer-21 <ajmanibhupa@gmail.com>
2026-09-05 17:57:12 +0530

    Add main application class for Spring Boot

---
+++ StudentDatabaseApp/StudentApplication.java
@@ -0,0 +1,10 @@
+package org.example;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+public class StudentApplication
+{
+    public static void main(String args[])
+    {
+        SpringApplication.run(StudentApiApplication.class, args);
+    }
+}
